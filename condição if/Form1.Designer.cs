4d8ffd3617177fd54ff51da05ce87651b59f7898
Flavia Martins <flavia.martins1991@hotmail.com>
2021-03-03 22:19:52 -0300

    Concepts about conditionals - developer in C#

---
+++ condição if/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace condição_if
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.didigitaIdade = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnVerificar.Location = new System.Drawing.Point(128, 258);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(172, 64);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Location = new System.Drawing.Point(65, 163);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(308, 31);
+            this.txtIdade.TabIndex = 2;
+            // 
+            // didigitaIdade
+            // 
+            this.didigitaIdade.AutoSize = true;
+            this.didigitaIdade.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.didigitaIdade.Location = new System.Drawing.Point(110, 83);
+            this.didigitaIdade.Name = "didigitaIdade";
+            this.didigitaIdade.Size = new System.Drawing.Size(231, 29);
+            this.didigitaIdade.TabIndex = 3;
+            this.didigitaIdade.Text = "Digite a sua idade:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 402);
+            this.Controls.Add(this.didigitaIdade);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.btnVerificar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.Label didigitaIdade;
+    }
+}
+
---
+++ condição if/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace condição_if
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            int idade;
+            idade = Convert.ToInt32(txtIdade.Text);
+
+            if (idade < 18)
+                MessageBox.Show("Voce ainda é menor de idade", "Mensagem");
+            else
+                MessageBox.Show("Voc ja é maior de idade", "Mensagem");
+
+        }
+    }
+}
